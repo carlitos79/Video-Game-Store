932fd6a826668f7564ec9602a8ed8253d5746f31
Carlos Peñaloza <carlitos792001@yahoo.es>
2018-01-19 09:50:12 +0100

    information control in order

--- GameStore/Models/Order.cs
+++ GameStore/Models/Order.cs
@@ -10,21 +10,30 @@ namespace GameStore.Models
     {
         public int OrderId { get; set; }
         public string OrderShoppingCartId { get; set; }
+        [Required]
         [Display(Name = "First Name")]
         [DataType(DataType.Text)]
         public string FirstName { get; set; }
+        [Required]
         [Display(Name = "Last Name")]
         [DataType(DataType.Text)]
         public string LastName { get; set; }
+        [Required]
         [Display(Name = "Identity Number")]
         [DataType(DataType.Text)]
         public string IdentityNo { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         [Display(Name = "Postal Code")]
         [DataType(DataType.Text)]
         public string PostalCode { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Email is not valid.")]
         public string Email { get; set; }
+        [Required]
         public string Phone { get; set; }
         public decimal Total { get; set; }
         [Display(Name = "Order Date")]
